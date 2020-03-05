d7bac618e5ad3ef6d6838389b803ec7e4a245ac4
Abidemi Kusimo <askusimo@uclan.ac.uk>
2020-03-05 14:03:34 +0000

    canvas id declared as context and object as ctx

---
+++ js/abi.js
@@ -0,0 +1,7 @@
+var context = document.getElementById("canvas")
+var ctx = context.getContext("2d")
+
+canvasHeight = context.height  // Canvas Height
+canvasWidth = context.width    // Canvas Width
+centerY = canvasHeight / 2     // Center Y
+centerX = canvasWidth / 2      
