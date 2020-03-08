08b1040243d7e5eb6eba008ef24c51c21838109e
harryjolly <60742091+harryjolly@users.noreply.github.com>
2020-03-08 11:07:40 +0000

    adding eyes

--- js/harry.js
+++ js/harry.js
@@ -1,7 +1,29 @@
+var context = document.getElementById("canvas")
+var ctx = context.getContext("2d")
+
 //to do eyes
+
+function makeArc(a, b, c, d, fill = "") {
+  ctx.beginPath();
+  ctx.arc(a, b, c, d, 2 * Math.PI);
+  ctx.fillStyle = fill;
+  ctx.fill();
+  ctx.closePath();
+  ctx.lineWidth = 5;
+  ctx.stroke();
+};
 
 // to do nose
 
 // to do smile 
 
 // to do dimples 
+
+// left eye
+makeArc(285, 355, 65, 0, "white");
+makeArc(288, 355, 10, 0, "black");
+
+// right eye
+// eye is positioned lower so the nose is overlapping
+makeArc(115, 355, 65, 0, "white");
+makeArc(112, 355, 10, 0, "black");
