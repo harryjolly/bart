c4f7703583a9b229c8092b95a0386bf0d83cf04f
harryjolly <60742091+harryjolly@users.noreply.github.com>
2020-03-08 11:12:52 +0000

    added smile and dimples

--- js/harry.js
+++ js/harry.js
@@ -27,6 +27,14 @@ function semicircle(a, b, c, d, fill = "") {
 
 // to do smile 
 
+function curvedline(x, y, a, b, c, d) {
+  ctx.beginPath();
+  ctx.moveTo(x, y);
+  ctx.quadraticCurveTo(a, b, c, d);
+  ctx.stroke();
+
+}
+
 // to do dimples 
 
 // left eye
@@ -42,3 +50,15 @@ semicircle(202, 405, 40, 5.2, "#ffd200");
 // eye is positioned lower so the nose is overlapping
 makeArc(115, 355, 65, 0, "white");
 makeArc(112, 355, 10, 0, "black");
+
+//smile
+// number 1 and 2 are starting position of line (x,y)
+// number 3 and 4position of control point to curve the line (x,y)
+// number 5 and 6 are final position of line position (x,y)
+curvedline(302, 485, 200, 540, 98, 485);
+
+// left dimple
+curvedline(300, 500, 310, 480, 300, 470);
+
+// right dimple
+curvedline(100, 500, 90, 480, 100, 470);
